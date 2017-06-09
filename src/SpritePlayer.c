105111c2b4398ea7772fc8a4a873d1996fe5cf76
Henrik <dev@infectedbytes.com>
2017-06-10 00:20:30 +0200

    fixed solution file, added simple movement and collision

--- src/SpritePlayer.c
+++ src/SpritePlayer.c
@@ -1,11 +1,25 @@
 #pragma bank=2
 #include "SpritePlayer.h"
+#include "Keys.h"
+#include "SpriteManager.h"
 UINT8 bank_SPRITE_PLAYER = 2;
 
 void Start_SPRITE_PLAYER() {
 }
 
 void Update_SPRITE_PLAYER() {
+	if (KEY_PRESSED(J_UP)) {
+		TranslateSprite(THIS, 0, -1);
+	}
+	if (KEY_PRESSED(J_DOWN)) {
+		TranslateSprite(THIS, 0, 1);
+	}
+	if (KEY_PRESSED(J_LEFT)) {
+		TranslateSprite(THIS, -1, 0);
+	}
+	if (KEY_PRESSED(J_RIGHT)) {
+		TranslateSprite(THIS, 1, 0);
+	}
 }
 
 void Destroy_SPRITE_PLAYER() {
--- src/StateGame.c
+++ src/StateGame.c
@@ -10,6 +10,9 @@ UINT8 bank_STATE_GAME = 2;
 #include "SpriteManager.h"
 
 extern UINT8 n_sprite_types;
+
+UINT8 collision_tiles[] = { 1, 0 };
+
 void Start_STATE_GAME() {
 	UINT8 i;
 
@@ -22,7 +25,7 @@ void Start_STATE_GAME() {
 	scroll_target = SpriteManagerAdd(SPRITE_PLAYER, 50, 50);
 
 	InitScrollTiles(0, 2, tiles, 3);
-	InitScroll(mapWidth, mapHeight, map, 0, 0, 3);
+	InitScroll(mapWidth, mapHeight, map, collision_tiles, 0, 3);
 	SHOW_BKG;
 }
 
