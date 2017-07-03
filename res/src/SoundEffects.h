fa50dd10ce1e2ecd140fe67809d761ff7031b2c0
StonyyBird <garbage.mails@gmx.de>
2017-07-03 15:49:40 +0200

    add player grounded (not velcro!) sound effect

--- res/src/SoundEffects.h
+++ res/src/SoundEffects.h
@@ -6,6 +6,7 @@
 #define PLAYFX_player_jump         PlayFx(CHANNEL_1, 20, 0x56, 0x81, 0xF2, 0x92, 0x84)
 #define PLAYFX_player_heal         PlayFx(CHANNEL_4, 20, 0x06, 0xF2, 0x7D, 0xC0)
 #define PLAYFX_player_damage       PlayFx(CHANNEL_4, 20, 0x06, 0xF6, 0x3F, 0xC0)
+#define PLAYFX_player_grounded     PlayFx(CHANNEL_1, 20, 0x19, 0x81, 0xF2, 0x73, 0x86)
 
 /* helper macro to play a sound effect with symbolic name
  * example: PLAYFX(player_jump)
