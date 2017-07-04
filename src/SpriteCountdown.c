b819f11036cbce1cd9b1b3a16872e0043f2d0f08
StonyyBird <garbage.mails@gmx.de>
2017-07-04 21:10:08 +0200

    adding countdown sounds, first tick sound seems to be played too early

--- src/SpriteCountdown.c
+++ src/SpriteCountdown.c
@@ -2,6 +2,8 @@
 #include "SpriteCountdown.h"
 #include "SpriteManager.h"
 #include "SpritePlayer.h"
+#include "Sound.h"
+#include "../res/src/SoundEffects.h"
 
 UINT8 bank_SPRITE_COUNTDOWN = 2;
 
@@ -14,9 +16,17 @@ void Start_SPRITE_COUNTDOWN() {
 }
 
 void Update_SPRITE_COUNTDOWN() {
-	if (countdownTimer++ == FRAMES_PER_SECOND && THIS->current_frame < 3) {
-		countdownTimer = 0;
-		THIS->current_frame++;
+	if (THIS->current_frame != 3) {
+		if (countdownTimer++ == FRAMES_PER_SECOND) {
+			countdownTimer = 0;
+			THIS->current_frame++;
+
+			if (THIS->current_frame == 3) {
+				PLAYFX(countdown_start);
+			} else {
+				PLAYFX(countdown_tick);
+			}
+		}
 	}
 }
 
