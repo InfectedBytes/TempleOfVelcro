55ecfb6ed6aff862ce215fcad37998298ab59108
Henrik <dev@infectedbytes.com>
2017-07-05 19:21:25 +0200

    fixed naming

--- res/src/tutorialTiles.h
+++ res/src/tutorialTiles.h
@@ -24,8 +24,8 @@
 
 
 /* Bank of tiles. */
-#define TileLabelBank 0
+#define tutorialTilesBank 0
 /* Start of tile array. */
-extern unsigned char TileLabel[];
+extern unsigned char tutorialTiles[];
 
 /* End of TUTORIALTILES.H */
