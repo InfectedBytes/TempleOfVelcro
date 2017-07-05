d0b7451df97ef41107a5ca75a0a4bb18183163e4
Dima <Dima>
2017-07-05 17:05:13 +0200

    Tilesets für Credits, Tutorial und Schwierigkeits-Auswahl-Screen hinzugefügt. Alte Versionen entfernt. Dateinamen optimiert.

--- res/src/tiles_tutorial.h
+++ res/src/tiles_tutorial.h
@@ -1,6 +1,6 @@
 /*
 
- TITLE.H
+ TILES_TUTORIAL.H
 
  Include File.
 
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 120
+  Tiles                : 0 to 0
 
   Palette colors       : None.
   SGB Palette          : None.
@@ -28,4 +28,4 @@
 /* Start of tile array. */
 extern unsigned char TileLabel[];
 
-/* End of TITLE.H */
+/* End of TILES_TUTORIAL.H */
