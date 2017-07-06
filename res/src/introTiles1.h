90c6b356bb6695bb15090f5046b6cf2c1b091ff6
Henrik <dev@infectedbytes.com>
2017-07-06 16:00:25 +0200

    fixed export

--- res/src/introTiles1.h
+++ res/src/introTiles1.h
@@ -10,7 +10,7 @@
   Compression          : None.
   Counter              : None.
   Tile size            : 8 x 8
-  Tiles                : 0 to 56
+  Tiles                : 0 to 88
 
   Palette colors       : None.
   SGB Palette          : None.
