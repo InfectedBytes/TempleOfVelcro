7562c0317e89adebfc25a0c62e0b9a987bc7ba02
StonyyBird <garbage.mails@gmx.de>
2017-07-06 07:16:42 +0200

    fixing gameover

--- res/src/gameoverTiles.h
+++ res/src/gameoverTiles.h
@@ -24,8 +24,8 @@
 
 
 /* Bank of tiles. */
-#define TileLabelBank 0
+#define gameoverTilesBank 0
 /* Start of tile array. */
-extern unsigned char TileLabel[];
+extern unsigned char gameoverTiles[];
 
 /* End of GAMEOVERTILES.H */
--- src/StateGameover.c
+++ src/StateGameover.c
@@ -5,22 +5,15 @@ UINT8 bank_STATE_GAMEOVER = 4;
 #include "ZGBMain.h"
 #include "Keys.h"
 #include "Scroll.h"
-#include "../res/src/font.h"
 #include "../res/src/gameoverMap.h"
-#include "../res/src/tiles.h"
+#include "../res/src/gameoverTiles.h"
 
 extern UINT8* gameover_mod_Data[];
 
 void Start_STATE_GAMEOVER() {
 	HIDE_WIN;
-	InitScrollTiles(0, 32, tiles, 3);
+	InitScrollTiles(0, 74, gameoverTiles, 4);
 	InitScroll(gameoverMapWidth, gameoverMapHeight, gameoverMap, 0, 0, 4);
-	RefreshScroll();
-	INIT_FONT(font, 3, PRINT_BKG);
-	PRINT_POS(6, 8);
-	Printf("Gameover");
-	PRINT_POS(3, 11);
-	Printf("Press any key");
 	BGP_REG = PAL_DEF(0, 1, 2, 3);
 	SHOW_BKG;
 
