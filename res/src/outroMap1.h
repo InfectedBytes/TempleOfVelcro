fd83298a14281ad8d1e8252fb3cab8e94d7efc83
Henrik <dev@infectedbytes.com>
2017-07-07 18:04:31 +0200

    added outro 1

--- res/src/outroMap1.h
+++ res/src/outroMap1.h
@@ -8,7 +8,7 @@
    Section       : 
    Bank          : 0
    Map size      : 20 x 18
-   Tile set      : title.gbr
+   Tile set      : outro1.gbr
    Plane count   : 1 plane (8 bits)
    Plane order   : Tiles are continues
    Tile offset   : 0
